fe8e1181acdeb0145ad79062e2c09702b5b80e13
Diego Palacios <diego.fernando.palacios@correounivalle.edu.co>
2024-12-21 10:42:45 -0500

    se agregan botones a la homepage

--- vista/src/components/Footer.jsx
+++ vista/src/components/Footer.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
     return (
-        
         <footer className='footer-styled'>
-           
+            <div className="footer-center">
+                <button className="btn-participar">PARTICIPAR</button>
+                <button className="btn-ver-ganadores">ver ganadores</button>
+            </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
