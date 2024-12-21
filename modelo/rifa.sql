831a9d5ede89402c49bd1ba25a6fd6122bfd27c1
Diego Palacios <diego.fernando.palacios@correounivalle.edu.co>
2024-12-21 12:44:36 -0500

    base de datos inicializada con las tablas y los premios

--- modelo/rifa.sql
+++ modelo/rifa.sql
@@ -4,7 +4,7 @@ USE rifa;
 -- Crear la tabla clientes
 CREATE TABLE clientes (
     identificacion INT PRIMARY KEY, -- Llave primaria sin autoincremento
-    nombrePremio VARCHAR(255),     -- Campo para el nombre del premio, tipo string
+    nombreUsuario VARCHAR(255),     -- Campo para el nombre del usuario, tipo string
     usuarioHabilitado BOOLEAN,     -- Campo booleano para indicar si el usuario está habilitado
     haParticipado BOOLEAN          -- Campo booleano para indicar si ha participado
 );
@@ -18,3 +18,15 @@ CREATE TABLE premios (
     CONSTRAINT fk_idGanador FOREIGN KEY (idGanador) REFERENCES clientes(identificacion)
 );
 
+-- Insertar premios en la tabla premios
+INSERT INTO premios (idPremio, nombrePremio, disponible)
+VALUES 
+(1, 'olla arrocera', TRUE),
+(2, 'parlante', TRUE),
+(3, 'ventilador 3 en 1', TRUE),
+(4, 'olla a presion', TRUE),
+(5, 'horno microondas', TRUE),
+(6, 'licuadora', TRUE),
+(7, 'air fryer', TRUE),
+(8, 'cafetera', TRUE),
+(9, 'juego de ollas', TRUE);
--- vista/src/components/Header.jsx
+++ vista/src/components/Header.jsx
@@ -4,6 +4,7 @@ import LogoNavidad from '../assets/FotosRetocadas/LogoNavidad.png';
 const Header = () => {
     return (
         <header className="header-styled">
+            <h1 className="header-title">GRAN RIFA NAVIDEÑA</h1>
             <img src={LogoNavidad} alt="Navidad Logo" className="navidad-logo" />
         </header>
     );
