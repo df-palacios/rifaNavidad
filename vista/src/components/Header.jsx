e8ad92927c4a3116cd1672a8f9cd52a5351bafbf
Diego Palacios <diego.fernando.palacios@correounivalle.edu.co>
2024-12-23 01:54:01 -0500

    minor comments

--- vista/src/components/Header.jsx
+++ vista/src/components/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import HeaderImage from '../assets/FotosRetocadas/headerImage.png';
 
+/**
+ * Renders the header component for the application.
+ * The header includes an image with the class "header-image".
+ * The header is wrapped in a "header-styled" class.
+ */
 const Header = () => {
     return (
         <header className="header-styled">
